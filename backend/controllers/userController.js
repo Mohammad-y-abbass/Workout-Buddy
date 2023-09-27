b311eb85adca2db8d6c3128af5995abab024cac2
Mohammad-y-abbass <144027220+Mohammad-y-abbass@users.noreply.github.com>
2023-09-27 17:39:26 +0300

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,1 @@
-const User = require("../models/userModel");
 
-
-
-const signupUser = async (req, res) => {
-  res.json({ message: "signup" });
-};
-
-const loginUser = async (req, res) => {
-  res.json({ message: "login" });
-};
-
-module.exports = {
-  signupUser,
-  loginUser,
-};
