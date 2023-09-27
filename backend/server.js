3e6dfbb49b3010b583436f58759343d8d41ad820
Mohammad-y-abbass <144027220+Mohammad-y-abbass@users.noreply.github.com>
2023-09-27 17:34:06 +0300

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const workoutsRouter = require("./routes/workout");
+const userRoutes = require('./routes/user');
+
+const mongoose = require("mongoose");
+const { bodyParser } = require("json-server");
+
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/workouts", workoutsRouter);
+app.use("/api/user", userRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
